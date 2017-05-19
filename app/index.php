5f78688bcc4e0613b4e884e6a2b97cf83742689c
Alexandr <buralex89@gmail.com>
2017-05-19 03:32:39 +0300

    4.2 closure

---
+++ app/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Template</title>
+	<meta name="description" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="icon" type="image/png" href="favicon.png">
+	<link rel="stylesheet" href="/css/main.min.css">
+	<script src="/js/jquery.min.js"></script>
+</head>
+
+<body class="loaded">
+
+<div class="main-wrapper o-green">
+
+    <!-- HEADER -->
+    <header class="main-header ">
+        <div class="container-fluid ">
+            <div class="row">
+
+                <!-- SLIDER -->
+                <div class="main-header__slider">
+                    <a href="#" class="control_next">>></a>
+                    <a href="#" class="control_prev"><</a>
+                    <ul>
+                        <li>SLIDE 1</li>
+                        <li style="background: #aaa;">SLIDE 2</li>
+                        <li>SLIDE 3</li>
+                        <li style="background: #aaa;">SLIDE 4</li>
+                    </ul>
+                </div>
+
+                <div class="slider_option">
+                    <input type="checkbox" id="checkbox">
+                    <label for="checkbox">Autoplay Slider</label>
+                </div>
+                <!-- EOF SLIDER -->
+
+                <nav class="main-nav o-green">
+                    <div class="main-nav__header">
+                        <button type="button" class="main-nav__toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="main-nav__brand" href="#myPage">Golden</a>
+                    </div>
+                    <div class="main-nav__navbar" >
+                        <ul class="main-nav__content">
+                            <li><a href="#home">HOME</a></li>
+                            <li class="dropdown">
+                                <a href="#services">SERVICES</a>
+                                <ul class="sub-menu">
+                                    <li><a href="#">Service-1</a></li>
+                                    <li class="dropdown">
+                                        <a href="#">Service-2</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="#">Subservice 1</a></li>
+                                            <li><a href="#">Subservice 2</a></li>
+                                            <li><a href="#">Subservice 3</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Service-3</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#options">OPTIONS</a></li>
+                            <li><a href="#contact">CONTACT</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <!-- EOF HEADER  -->
+
+    <!-- CONTENT -->
+    <main class="content">
+
+        <div class="wrapper">
+
+        </div>
+
+
+    </main>
+    <!-- EOF CONTENT -->
+
+</div>
+
+<!-- FOOTER -->
+<footer class="main-footer o-blue">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="main-footer__social col-sm-6 col-sm-push-3 o-blue">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa fa-pinterest-p" aria-hidden="true"></i>
+                <i class="fa fa-google-plus" aria-hidden="true"></i>
+            </div>
+            <div class="main-footer__copyright col-sm-3 col-sm-pull-6 o-red">
+                &copy; Copyright 2017
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- EOF FOOTER  -->
+
+
+
+<div class="icon-load"></div>
+
+<script src="js/main.js"></script>
+<!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.js" ></script>-->
+
+<script>
+    $(document).ready(function(){
+//
+//        $('.slider-blagoy').slick({
+//
+//            dots: true,
+//            infinite: true
+//        });
+
+        stickyFooter('.main-footer', '.main-wrapper');
+        //navbar('.navbar__collapse', '.navbar-toggle');
+
+
+
+    });
+</script>
+</body>
+</html>
---
+++ app/js/main.js
@@ -0,0 +1,373 @@
+ "use strict";
+
+
+ /*------------------------------------------------------------------------------
+
+                        COUNTER (COOKIE)
+
+  ------------------------------------------------------------------------------*/
+ (function() {
+
+     function setCookie(cname, cvalue, exdays) {
+         var d = new Date();
+         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+         var expires = "expires="+d.toUTCString();
+         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+     }
+
+     function getCookie(cname) {
+         var name = cname + "=";
+         var ca = document.cookie.split(';'); //cookie array
+         for(var i = 0; i < ca.length; i++) {
+             var c = ca[i];
+             while (c.charAt(0) == ' ') {
+                 c = c.substring(1);
+             }
+             if (c.indexOf(name) == 0) {
+                 return c.substring(name.length, c.length);
+             }
+         }
+         return "";
+     }
+
+
+// checking 3 times
+     function showModal() {
+
+         if (getCookie("aa") != "" ) {
+
+             var counter = Number(getCookie("aa"));
+
+             if (counter < 3) {
+                 counter++;
+                 setCookie("aa", counter, "30");
+                 //document.querySelector(".counter").classList.add('hidden');
+                 var el = document.querySelector(".counter");
+                 el.classList.add('visible');
+                 //console.log(document.querySelector(".counter"));
+             }
+         } else {
+             setCookie("aa", 1, "30");
+             var el = document.querySelector(".counter");
+             el.classList.add('visible');
+         }
+         return counter;
+     }
+//console.log(showModal());
+     //document.cookie = "aa=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+ })();
+
+/*---------------------------------------------------------------
+
+						CUSTOM
+						
+----------------------------------------------------------------*/
+
+document.addEventListener('DOMContentLoaded', function(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+        document.querySelector('body').classList.add('ios');
+    };
+    document.querySelector('body').classList.remove('loaded');
+    //setTimeout(function(){document.querySelector('body').classList.remove('loaded'); }, 2000);
+});
+
+
+$(function(){
+	/* placeholder*/	   
+	$('input, textarea').each(function(){
+ 		var placeholder = $(this).attr('placeholder');
+ 		$(this).focus(function(){ $(this).attr('placeholder', '');});
+ 		$(this).focusout(function(){			 
+ 			$(this).attr('placeholder', placeholder);  			
+ 		});
+ 	});
+	/* placeholder*/
+});
+
+ /*---------------------------------------------------------------
+
+                         SLIDER
+
+  ----------------------------------------------------------------*/
+
+(function() {
+
+    document.addEventListener('DOMContentLoaded', function(){
+
+        // window.time = 0; //global declaration
+        // function autorefresh() {
+        //     var isChecked = document.getElementById("checkbox").checked;
+        //     if (isChecked == true) {
+        //         time = setInterval(function () {
+        //             moveRight();
+        //         }, 1000);
+        //     } else if (isChecked == false) {
+        //         clearInterval(time);
+        //     }
+        // }
+        // autorefresh();
+        // document.getElementById('checkbox').addEventListener('click', autorefresh);
+
+        var autoSlide = (function () {
+            var flag = true;
+
+            return function () {
+                if (flag) {
+                    console.log('dddd');
+                    var myVar = setInterval(function(){ console.log('dddd') }, 1000);
+                    flag = false;
+                } else {
+                    console.log('ccc');
+                    flag = true;
+                    clearInterval(myVar);
+                }
+                //return counter += 1
+            }
+        })();
+        document.getElementById('checkbox').addEventListener('click', autoSlide);
+
+
+
+        // var autoSlide = (function () {
+        //     var flag = true;
+        //
+        //     return function () {
+        //         if (flag) {
+        //             console.log('dddd');
+        //             //var myVar = setInterval(function(){ console.log('dddd') }, 1000);
+        //             flag = false;
+        //         } else {
+        //             console.log('ccc');
+        //             flag = true;
+        //             //clearInterval(myVar);
+        //         }
+        //         //return counter += 1
+        //     }
+        // })();
+        // autoSlide();
+        // autoSlide();
+        // autoSlide();
+
+
+
+
+        // var myVar = setInterval(function(){ console.log('dddd') }, 1000);
+        //
+        // function myTimer() {
+        //     console.log('ddd');
+        // }
+        //
+        // function myStopFunction() {
+        //     clearInterval(myVar);
+        // }
+
+        var slideCount = $('.main-header__slider ul li').length;
+        var slideWidth = $('.main-header__slider ul li').width();
+        var slideHeight = $('.main-header__slider ul li').height();
+        var sliderUlWidth = slideCount * slideWidth;
+
+        $('.main-header__slider').css({ width: slideWidth, height: slideHeight });
+
+        $('.main-header__slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+        $('.main-header__slider ul li:last-child').prependTo('.main-header__slider ul');
+
+        function moveLeft() {
+            $('.main-header__slider ul').animate({
+                left: + slideWidth
+            }, 200, function () {
+                $('.main-header__slider ul li:last-child').prependTo('.main-header__slider ul');
+                $('.main-header__slider ul').css('left', '');
+            });
+        };
+
+        function moveRight() {
+            $('.main-header__slider ul').animate({
+                left: - slideWidth
+            }, 200, function () {
+                $('.main-header__slider ul li:first-child').appendTo('.main-header__slider ul');
+                $('.main-header__slider ul').css('left', '');
+            });
+        };
+
+        $('a.control_prev').click(function () {
+            moveLeft();
+        });
+
+        $('a.control_next').click(function () {
+            moveRight();
+        });
+    });
+    
+})();
+
+
+
+ /*---------------------------------------------------------------
+
+                         NAVBAR
+
+  ----------------------------------------------------------------*/
+ (function() {
+
+     var b = document.querySelector('.main-nav__toggle');
+     //var num = 0;
+
+     b.addEventListener("click", function() {
+
+         document.querySelector('.main-nav__navbar').classList.toggle('opened');
+
+     });
+
+     $( '.dropdown' ).hover(
+         function(){
+             $(this).children('.sub-menu').slideDown(200, function () {
+                 if ($( this ).css( 'display') === 'none' ) {
+                     $(this).removeAttr('style');
+                 }
+             });
+         },
+         function(){
+             $(this).children('.sub-menu').slideUp(200, function () {
+                 if ($( this ).css( 'display') === 'none' ) {
+                     $(this).removeAttr('style');
+                 }
+             });
+         }
+     );
+     
+ })();
+
+
+
+/*------------------------------------------------------------------------------
+
+						DEBOUNCE
+						
+------------------------------------------------------------------------------*/
+Function.prototype.debounce = function (milliseconds) {
+    var baseFunction = this,
+        timer = null,
+        wait = milliseconds;
+
+    return function () {
+        var self = this,
+            args = arguments;
+
+        function complete() {
+            baseFunction.apply(self, args);
+            timer = null;
+        }
+
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        timer = setTimeout(complete, wait);
+    };
+};
+/* -----------------------------------------------------------------------------
+
+                      STICKY FOOTER     
+  
+----------------------------------------------------------------------------- */
+
+function stickyFooter(footerContainer, wrapCont) {
+    
+    function stick() {
+        var footerHeight = document.querySelector(footerContainer).offsetHeight;
+        document.querySelector(wrapCont).style.cssText = "padding-bottom: " + footerHeight + "px;";
+    }
+	
+   window.addEventListener("load", function(event) { stick() }.debounce(10));
+   window.addEventListener("resize", function(event) { stick() }.debounce(10));
+   
+}
+/*---------------------------------------------------------------
+
+						attach
+						
+----------------------------------------------------------------*/
+
+(function() {
+
+  // var a = document.querySelector('.attach__button');
+  // var num = 0;
+  //
+  // a.addEventListener("change", function() {
+  //
+  //   for (var i = 0; i < this.files.length; i++) num++;
+  //
+  //   document.querySelector('.attach__val').innerHTML = num;
+  //
+  // });
+  
+})();
+
+/*---------------------------------------------------------------
+
+						SEND REQUEST
+						
+----------------------------------------------------------------*/
+
+(function() {
+  
+  $("#mainform").on('submit' , function(e) {
+    //alert('fff');
+    $('.icon-load').show();
+    var formObj = $(this);
+    //var formURL = formObj.attr("action");
+    var formData = new FormData(this);
+    $.ajax({
+    url: "send.php",
+    type: 'POST',
+    data:  formData,
+    mimeType:"multipart/form-data",
+    contentType: false,
+    cache: false,
+    processData:false,
+    success: function(data, textStatus, jqXHR) {
+      $('.icon-load').hide();
+       $('#contactModal').modal('show');
+
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+       
+    }          
+  });
+      return false;
+      e.preventDefault(); //Prevent Default action. 
+  }); 
+  
+  
+  /*----------------------- callback form -----------------------------*/
+  $("#secondForm").on('submit' , function(e) {
+    //alert('fff');
+    $('.icon-load').show();
+    var formObj = $(this);
+    //var formURL = formObj.attr("action");
+    var formData = new FormData(this);
+    $.ajax({
+    url: "send.php",
+    type: 'POST',
+    data:  formData,
+    mimeType:"multipart/form-data",
+    contentType: false,
+    cache: false,
+    processData:false,
+    success: function(data, textStatus, jqXHR) {
+      $('.icon-load').hide();
+       $('#contactModal').modal('show');
+
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+       
+    }          
+  });
+      return false;
+      e.preventDefault(); //Prevent Default action. 
+  }); 
+  /*----------------------- eof callback form -----------------------------*/
+
+})();
+
