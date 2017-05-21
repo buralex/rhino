92c0e67a4ab615dbc59011935870193dfb6f4c30
Alexandr <buralex89@gmail.com>
2017-05-21 03:17:30 +0300

    only full width slider

--- app/js/slider.js
+++ app/js/slider.js
@@ -35,42 +35,31 @@ Function.prototype.debounce = function (milliseconds) {
                                 SLIDER
 
  ------------------------------------------------------------------------------*/
-    //$(document).ready(function(){
+
     document.addEventListener('DOMContentLoaded', function(){
         function responsiveSlider() {
 
+            var slideInterval = setInterval(moveRight, 4000);
             var playing = true;
             var pauseButton = document.getElementById('pause');
             var slideCount = document.querySelectorAll('#slider ul li').length;
-            var windWidth = document.documentElement.clientWidth;
-            var slideWidth = document.querySelector('#slider').offsetWidth;
+            var slideWidth = document.documentElement.clientWidth;
             var slideHeight = document.querySelector('#slider ul li').offsetHeight;
             var UlWidth = slideCount * slideWidth;
             var sliderBox = document.querySelector("#slider");
             var ul = document.querySelector("#slider ul");
             var liArray = document.querySelectorAll("#slider ul li");
 
-
-
-            // if ( slideWidth > windWidth) {
-            //     slideWidth = windWidth ;
-            //     console.log('kfdjslfkjls');
-            // }
-            //slideWidth = (windWidth == slideWidth)? windWidth : slideWidth;
-
-            //var slideInterval = setInterval(moveRight, 4000);
-            //ul.style.opacity = 0;
-
             function pauseSlideshow(){
                 pauseButton.innerHTML = '&#9658;'; // play character
                 playing = false;
-                //clearInterval(slideInterval);
+                clearInterval(slideInterval);
             }
 
             function playSlideshow(){
                 pauseButton.innerHTML = '&#10074;&#10074;'; // pause character
                 playing = true;
-                //slideInterval = setInterval(moveRight, 4000);
+                slideInterval = setInterval(moveRight, 4000);
             }
 
             pauseButton.addEventListener( "click" , function() {
@@ -79,78 +68,53 @@ Function.prototype.debounce = function (milliseconds) {
                 }	else { playSlideshow();}
             });
 
-            console.log(slideWidth, windWidth, slideCount);
+            console.log(slideWidth, slideCount);
 
             //moving last image in the beginning
-            // ul.insertBefore(document.querySelector("#slider ul li:last-child"), ul.firstChild);
-            //
-            // sliderBox.style.cssText = "width: " + slideWidth + "px;" + "height:" + slideHeight + "px;";
-            //
-            // //li.style.cssText = "width: " + slideWidth + "px;";
-            //
-            // ul.style.cssText = "width: " + UlWidth + "px;" + "margin-left:" + (-slideWidth)
-            //                     + "px;" + "opacity: 1;";
-            //
-            // liArray.forEach(function (el) {
-            //     el.style.cssText = "width: " + slideWidth + "px;";
-            //     //console.log(el);
-            // }, false);
+            ul.insertBefore(document.querySelector("#slider ul li:last-child"), ul.firstChild);
+
+            sliderBox.style.cssText = "width: " + slideWidth + "px;" + "height:" + slideHeight + "px;";
+
+            ul.style.cssText = "width: " + UlWidth + "px;" + "margin-left:" + (-slideWidth)
+                                + "px;" + "opacity: 1;";
+
+            liArray.forEach(function (el) {
+                el.style.cssText = "width: " + slideWidth + "px;";
+            }, false);
 
             window.addEventListener("resize", function(event) {
-                playing = true;
-                pauseButton = document.getElementById('pause');
-                slideCount = document.querySelectorAll('#slider ul li').length;
-                windWidth = document.documentElement.clientWidth;
-                slideWidth = document.querySelector('#slider').offsetWidth;
-                if (slideWidth >= windWidth) {slideWidth = windWidth}
-
+                slideWidth = document.documentElement.clientWidth;
                 slideHeight = document.querySelector('#slider ul li').offsetHeight;
                 UlWidth = slideCount * slideWidth;
-                sliderBox = document.querySelector("#slider");
-                ul = document.querySelector("#slider ul");
-                liArray = document.querySelectorAll("#slider ul li");
-                //responsiveSlider()
 
-                // ul.insertBefore(document.querySelector("#slider ul li:last-child"), ul.firstChild);
-                //
                 sliderBox.style.cssText = "width: " + slideWidth + "px;" + "height:" + slideHeight + "px;";
-                //
-                // //li.style.cssText = "width: " + slideWidth + "px;";
-                //
-                // ul.style.cssText = "width: " + UlWidth + "px;" + "margin-left:" + (-slideWidth)
-                //     + "px;" + "opacity: 1;";
-                //
-                // liArray.forEach(function (el) {
-                //     el.style.cssText = "width: " + slideWidth + "px;";
-                //     //console.log(el);
-                // }, false);
-                console.log(slideWidth, windWidth, slideCount);
+
+                ul.style.cssText = "width: " + UlWidth + "px;" + "margin-left:" + (-slideWidth)
+                    + "px;" + "opacity: 1;";
+
+                liArray.forEach(function (el) {
+                    el.style.cssText = "width: " + slideWidth + "px;";
+                }, false);
+
+                //console.log(slideWidth, slideCount);
             }.debounce(10));
 
             function moveLeft() {
-                // $('#slider ul').animate({
-                //     left: + slideWidth
-                // }, 2000, function () {
-                //     // $('#slider ul li:last-child').prependTo('#slider ul');
-                //
-                //     ul.insertBefore(document.querySelector("#slider ul li:last-child"), ul.childNodes[0]);
-                //     //list.insertBefore(newItem, list.childNodes[0]);
-                //     $('#slider ul').css('left', '');
-                // });
-                console.log('moveleft');
-                ul.insertBefore(document.querySelector("#slider ul li:last-child"), ul.childNodes[0]);
+                $('#slider ul').animate({
+                    left: + slideWidth
+                }, 2000, function () {
+                    ul.insertBefore(document.querySelector("#slider ul li:last-child"), ul.childNodes[0]);
+                    ul.style.left = "";
+                });
             };
 
             function moveRight() {
-                // $('#slider ul').animate({
-                //     left: - slideWidth
-                // }, 2000, function () {
-                //     // $('#slider ul li:first-child').appendTo('#slider ul');
-                //     ul.appendChild(document.querySelector("#slider ul li:first-child"));
-                //     $('#slider ul').css('left', '');
-                // });
-                console.log('moveright');
-                ul.appendChild(document.querySelector("#slider ul li:first-child"));
+                $('#slider ul').animate({
+                    left: - slideWidth
+                }, 2000, function () {
+                    ul.appendChild(document.querySelector("#slider ul li:first-child"));
+                    ul.style.left = "";
+                });
             };
 
 
@@ -162,7 +126,6 @@ Function.prototype.debounce = function (milliseconds) {
                 pauseSlideshow();
                 moveRight();
             });
-            //console.log(slideWidth);
 
         }
         responsiveSlider();
